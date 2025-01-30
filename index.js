72904d83df371afb28728f1b0d0087517a74178a
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-01-30 08:57:10 -0500

    Update index.js
    Password generator will no longer include commas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let passwordEl1 = document.getElementById("password-el1")
 let passwordEl2 = document.getElementById("password-el2")
@@ -11,26 +11,16 @@ function getRandomPassword() {
 }
 
 function generatePassword() {
-    let newPassword1 = []
-    let newPassword2 = []
-    
-    for (i= 0; i < 15; i++) {
-        newPassword1.push(characters[getRandomPassword()])
-    } 
-    passwordEl1.textContent = newPassword1
-    
-    for (i= 0; i < 15; i++) {
-        newPassword2.push(characters[getRandomPassword()])
-    } 
-    passwordEl2.textContent = newPassword2
+    passwordEl1.textContent = ""
+    passwordEl2.textContent = ""
+    for (i = 0; i < 15; i++) {
+        passwordEl1.textContent += characters[getRandomPassword()]
+    }
+    for (i = 0; i < 15; i++) {
+        passwordEl2.textContent += characters[getRandomPassword()]
+    }
 }
 
-function copyText() {
-    let copyText = document.getElementById("password-el1").innerText
-    copyText.select()
-    navigator.clipboard.writeText(copyText.value)
-    alert("Copied Password")
-}
 
 
 
